3c9cd0e24aec42f88fc5ed2aef31793d1a88da48
Piya Gehi <piya@orgnizedmess.net>
2026-09-14 15:23:17 +0400

    Add docs for system module

--- src/system.rs
+++ src/system.rs
@@ -1,56 +1,67 @@
-//! Client and models for system-level information about the EVE-NG instance.
+//! Types and client for system-level information about the EVE-NG instance.
 
 use crate::templates::{TemplateClient, TemplatesClient};
 use crate::utils::WireMap;
 use crate::{Client, Result};
+
 use serde::{Deserialize, Serialize};
 use std::collections::HashMap;
 
+/// Type to describe the status of the system running the EVE-NG instance.
 #[derive(Debug, Serialize, Deserialize)]
 pub struct SystemStatus {
-    /// cached memory (percentage)
+    /// Percentage of total RAM that is cached.
     pub cached: u8,
-    /// CPU usage (percentage)
+    /// Percentage of total CPU currently in use.
     pub cpu: u8,
-    /// Cpulimit status (enabled/disabled/unsupported)
+    /// Cpulimit status (enabled/disabled/unsupported).
     pub cpulimit: String,
-    /// Disk usage (percentage)
+    /// Percentage of total disk space currently in use.
     pub disk: u8,
-    // Running Docker wrappers
+    /// Number of running Docker nodes.
     pub docker: u32,
-    /// Running Dynamips wrappers
+    /// Number of running Dynamips nodes.
     pub dynamips: u32,
-    /// Running IOL wrappers
+    /// Number unning IOL nodes.
     pub iol: u32,
-    /// KSM status (enabled/disabled/unsupported)
+    /// KSM status (enabled/disabled/unsupported).
     pub ksm: String,
-    /// Memory usage (percentage)
+    /// Percentage of total RAM currently in use.
     pub mem: u8,
-    /// Running QEMU wrappers
+    /// Number of running QEMU nodes.
     pub qemu: u32,
+    /// Installed QEMU version on the host.
     pub qemu_version: String,
-    /// Swap usage (percentage)
+    /// Percentage of the total swap currently in use.
     pub swap: u8,
-    /// UKSM status (enabled/disabled/unsupported)
+    /// UKSM status (enabled/disabled/unsupported).
     pub uksm: String,
-    /// EVE-NG version
+    /// EVE-NG version.
     pub version: String,
-    /// Running VPCS wrappers
+    /// Number of running VPCS nodes.
     pub vpcs: u32,
 }
 
+/// Type to describe the status of the currently authenticated user.
 #[derive(Debug, Serialize, Deserialize)]
 pub struct AuthStatus {
+    /// Email address of the user.
     pub email: String,
-    /// Current folder
+    /// Path of the folder the user last viewed.
     pub folder: String,
+    /// A flag to indicate if the html5 console is in use.
     pub html5: i8,
+    /// Language select
     pub lang: String,
+    /// Full name of the user.
     pub name: String,
+    /// Role of the user.
     pub role: String,
+    /// Tenant ID of the user.
     pub tenant: u32,
+    /// Username of the user.
     pub username: String,
-    /// Current lab
+    /// Path of the lab currently open for the user.
     #[serde(skip_serializing_if = "Option::is_none")]
     pub lab: Option<String>,
 }
